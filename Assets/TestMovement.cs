8b776b8b2b83a0e2dd4413e66963ce43d0a5612a
reza300p <reza300p@gmail.com>
2020-02-17 18:23:30 +0000

    Initial files with some test
    Initializing project

---
+++ Assets/TestMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestMovement : MonoBehaviour
+{
+    
+    public GameObject gObject;
+    [Range (0,50.0F)]
+    public float speed = 0.5F;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Mathf.Abs(Input.GetAxis("Horizontal")) > 0.1F || Mathf.Abs(Input.GetAxis("Vertical")) > 0.1F ) {
+            Vector3 movement = new Vector3(Input.GetAxis("Horizontal"),0,Input.GetAxis("Vertical")) * speed * Time.deltaTime;
+            gObject.transform.Translate(movement);
+        }
+    }
+    
+    
+}
